a9f5e2924f10fc8520df7a5aa58728dca2ce67de
Gowtham-9615 <venkatagowthamreddy.polamreddy@wipro.com>
2024-08-21 11:13:11 +0530

    calculator

---
+++ dowerycal.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main(){
+    printf("Welcome to Dowery Calculator!\n");
+    printf("Enter Your Name: ");
+    char name[50];
+    for(int i=0; i<50; i++){
+        scanf("%c",name[i]);
+    }
+    printf("\nEnter your Age: ");
+    int age;
+    scanf("%d",&age);
+    printf("\nEnter Your Company: ");
+    char company[50];
+    for(int i=0; i<50; i++){
+        scanf("%c",company[i]);
+    }
+    printf("\nEnter Your Role: ");
+    char role[50];
+    for(int i=0; i<50;i++){
+        scanf("%c",role[i]);
+    }
+    printf("\nEnter Your Salary: ");
+    float salary;
+    scanf("%f",&salary);
+    printf("\nEnter Your Location: ");
+    char location[50];
+    for(int i=0; i<50; i++){
+        scanf("%c",location[i]);
+    }
+    printf("\nEnter Your 
+}
